a2e4bcead9f85f9add8eba5a5c075dcaf9275e42
Vangelis Spathonis <baggelisp.keph@gmail.com>
2021-12-23 13:25:38 +0200

    router / styles

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import logo from './assets/logo.svg';
-import './App.css';
+import { Routes, Route, Link } from "react-router-dom";
+import './styles/main.css';
+import Home from "./pages/Home";
+import About from "./pages/About";
+
 
 function App() {
   return (
     <div className="App">
-      <hi>hello word</hi>
-
+      <h1>Welcome to React Router!</h1>
+      <nav>
+       <Link to="/">Home </Link>
+        <Link to="/about">About</Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+      </Routes>
     </div>
   );
 }
